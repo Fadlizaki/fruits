62ed251a47e96f5fbdd30569043db36d2849515e
fadlizaki <coysiip25@gmail.com>
2023-08-29 22:22:20 +0700

    selesai

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -9,11 +9,7 @@
                 alt="" class="h-20 w-20" />
                 <div>
                     <h2 class="font-bold">{{ cart.name }}</h2>
-                    <p class="text-gray-700">{{ cart.cart_id }}</p>
-                </div>
-            </div>
-            <div class="flex items-center">
-                <div class="mx-4">
+                    <p class="text-gray-700"><div class="mx-4">
                   <span
                       class="cursor-pointer rounded-l bg-gray-100 py-1 px-3 duration-100 hover:bg-red-500 hover:text-blue-50"
                       @click="editKeranjang({ cart_id: cart.cart_id, type: 'minus' })"> - </span>
@@ -22,8 +18,19 @@
                     <span
                       class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
                       @click="editKeranjang({ cart_id: cart.cart_id, type: 'plus' })"> + </span>
+                      <button class="text-gray-600 transition hover:text-red-600" @click="deleteKeranjang(cart.cart_id)">
+                  <span class="sr-only">Remove item</span>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="h-4 w-4">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                  </svg>
+                </button>
+                </div></p>
                 </div>
-                
+            </div>
+            <div class="flex items-center">
                 <span class="font-bold">Rp.{{ cart.regular_price }}</span>
             </div>
         </div>
